9f14f191fcbdb5ddd72acc44d0984027554bf62d
Eugene-Okhryamkin <eugene.okhryamkin@gmail.com>
2021-06-08 10:08:19 +0300

    auth and logout

--- src/pages/Admin/sections/AdminHeader/Header.jsx
+++ src/pages/Admin/sections/AdminHeader/Header.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import { connect } from "react-redux";
+import propTypes from "prop-types";
+import {logout} from "Actions/logoutAction";
 
-const Header = () => (
-    <header>
-        <a href="" className="float-end mx-5 p-3 d-block">Выход</a>
-        <h5 className="p-3 d-block"><span className="float-right">Admin</span></h5>
-    </header>
-)
+const mapStateToProps = state => ({
+    user: state.user.user
+});
 
-export default Header;
+const mapDispatchToProps = dispatch => ({
+    logout: () => dispatch(logout())
+});
+
+const Header = props => {
+    const onHandleLogout = e => {
+        e.preventDefault();
+        const { logout } = props;
+        logout();
+        localStorage.removeItem("token");
+    }
+
+    return (
+        <header>
+            <a href="" className="float-end mx-5 p-3 d-block" onClick={onHandleLogout}>Выход</a>
+            <h5 className="p-3 d-block"><span className="float-right">{ props.user.sub }</span></h5>
+        </header>
+    )
+}
+
+Header.propTypes = {
+    user: propTypes.object.isRequired,
+    logout: propTypes.func.isRequired
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
